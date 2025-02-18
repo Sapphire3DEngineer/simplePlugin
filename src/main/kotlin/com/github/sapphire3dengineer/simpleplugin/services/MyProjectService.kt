65b411d0dc8d943d098c099f1141b1d6f7d9374e
GitHub Action <action@github.com>
2025-02-18 19:38:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/sapphire3dengineer/simpleplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sapphire3dengineer/simpleplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.sapphire3dengineer.simpleplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sapphire3dengineer.simpleplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
